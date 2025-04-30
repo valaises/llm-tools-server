ac040951cbbe4bccfd8c1e648f56055a1b952ce9
V4LER11 <work@valerii.cc>
2025-04-30 19:16:51 +0100

    updated message tok limit

--- src/core/globals.py
+++ src/core/globals.py
@@ -6,6 +6,6 @@ BASE_DIR: Path = Path(__file__).resolve().parent.parent.parent
 UPLOADS_DIR = BASE_DIR / "uploads"
 LOGS_DIR = BASE_DIR / "logs"
 
-MESSAGES_TOK_LIMIT = 64_000
+MESSAGES_TOK_LIMIT = 128_000
 
 LLM_PROXY_ADDRESS = os.environ["LLM_PROXY_ADDRESS"]
